830c178f3dc852e41be7040b95671629a1a04ffd
Elyse Klova <elyse.klova@gmail.com>
2017-01-24 17:55:54 -0600

    finalized event CRUD functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :days
   has_many :tasks, through: :days
+  has_many :events, through: :days
   has_secure_password
 
   validates :first_name, :presence => true
