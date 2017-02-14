5390a4b9a0a3223ce29f772e59fb66bc29dcd74d
Elyse Klova <elyse.klova@gmail.com>
2017-02-14 16:07:57 -0600

    completed previous/next day toggle functionality

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -18,4 +18,28 @@ class DaysController < ApplicationController
     erb :"/days/show"
   end
 
+  get "/days/:id/previous" do
+    current_day = Day.find_by_id(params[:id])
+    previous_day = Day.where("date < ? AND user_id = ?", current_day.date, current_user.id)[-2]
+    if previous_day
+      redirect "/days/#{previous_day.id}"
+    else
+      flash[:error] = "This is the earliest date in your BuJo 2.0 account"
+      redirect "/days/#{current_day.id}"
+    end
+  end
+
+  get "/days/:id/next" do
+    current_day = Day.find_by_id(params[:id])
+    next_day = Day.where("date > ? AND user_id = ?", current_day.date, current_user.id).first
+    if next_day
+      redirect "/days/#{next_day.id}"
+    else
+      flash[:error] = "BuJo 2.0 does not have a task list for the next day."
+      redirect "/days/#{current_day.id}"
+    end
+  end
+
+
+
 end
