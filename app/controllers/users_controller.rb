3869eded29b54595e5a358098402f243a836bb31
Elyse Klova <elyse.klova@gmail.com>
2017-05-13 17:31:29 -0500

    got validations working for creating a new user, broke updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,6 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !is_logged_in?
-      @user = User.new
       erb :'/users/create_user'
     else
       flash[:success] = "You're already logged in as #{current_user.first_name}"
@@ -47,36 +46,23 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:password] == params[:password_check]
-      @user = User.new(
+    @user = User.create(
         first_name: params[:first_name],
         last_name: params[:last_name],
         email: params[:email],
-        password: params[:password]
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
       )
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:success] = "Account created successfully. Welcome to BuJo 2.0!"
-        redirect '/days/today'
-      else
-        flash[:error] = @user.errors.full_messages
-        erb :"/users/create_user"
-      end
+    if !@user.id.nil?
+      session[:user_id] = @user.id
+      flash[:success] = "Account created successfully. Welcome to BuJo 2.0!"
+      redirect '/days/today'
     else
-      flash[:error] = "Password don't matc"
-      erb :"/users/create_user"
-
+      error_messages = []
+      @user.errors.messages.each {|key, value| error_messages << value.flatten}
+      flash[:error] = error_messages
+      redirect '/signup'
     end
-    #   session[:user_id] = user.id
-    #   flash[:success] = "Account created successfully. Welcome to BuJo 2.0!"
-    #   redirect '/days/today'
-    # elsif User.find_by(email: params[:email]) && params[:password] == params[:password_check]
-    #   flash[:error] = "There is already a user associated with that email address."
-    #   redirect '/signup'
-    # elsif params[:password] != params[:password_check]
-    #   flash[:error] = "Your password entries don't match."
-    #   redirect '/signup'
-    # end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
   has_many :tasks, through: :days
   has_many :events, through: :days
   has_many :migrations, through: :tasks
-  has_secure_password
+  has_secure_password validations: false
 
-  validates :first_name, :email, :last_name, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { message: "There is already a user associated with that email address"}
+  validates :password, confirmation: { message: "Both passwords must match"}
+  validates :first_name, :email, :last_name, presence: { message: "All fields must be filled out" }
+  validates :password, :password_confirmation, presence: { message: "All fields must be filled out" }
 end
